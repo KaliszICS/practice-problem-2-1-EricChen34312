f1fc5bc9e316fe646cf1d4ea7fe244f6ef48e0a2
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-03-10 03:22:16 +0000

    Solve Q1-Q4

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,75 @@
+import java.util.ArrayList;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		System.out.println(findAll(new double[] { 3.3, 3.5, 3.3, 3.2 }, 3.3));
 	}
 
-	
+	public static int find(int[] array, int number){
+		//do the thing
+		for(int i = 0; i < array.length;i++){
+			if (array[i] == number){
+				return i;
+			}
+		}
 
+		return -1;
+	}
+
+	public static int findLast(String[] array, String string){
+		//do the thing, but backwards
+		for(int i = array.length-1; i >= 0; i--){
+			if (array[i].equals(string)){
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public static int findSecond(char[] array, char character){
+		//store index for thing
+		int index = -1;
+
+		for(int i = 0; i < array.length; i++){
+			if(array[i] == character){
+
+				//if character never found before, set index to i
+				if(index == -1){
+					index = i;
+				} else {
+
+					//else return current i, as this would be the second occurence
+					return i;
+				}
+			}
+		}
+
+		//return index, would be -1 if value never found, or the first index if there isn't a second occurence
+		return index;
+	}
+
+	public static double[] findAll(double[] array, double number){
+		//make arraylist because i need dynamic
+		ArrayList<Integer> out = new ArrayList<>();
+		
+		//add indexes for every instance of number
+		for(int i = 0; i < array.length; i++){
+			if(array[i] == number){
+				out.add(i);
+			}
+		}
+
+		//add the output array (as a double[] for some reason)
+		double[] outArr = new double[out.size()];
+
+		//convert
+		for(int i = 0; i < out.size(); i++){
+			int temp = out.get(i);
+			outArr[i] = temp;
+		}
+
+		return outArr;
+	}
 }
